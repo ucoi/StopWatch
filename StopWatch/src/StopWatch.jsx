1f17e37178afd39213a43539f88da291a1e7e5e2
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-01-25 13:21:08 +0200

    made the buttons and the display of the Watch

--- StopWatch/src/StopWatch.jsx
+++ StopWatch/src/StopWatch.jsx
@@ -5,7 +5,33 @@ function StopWatch() {
   const intervalIdRef = useRef(null);
   const startTimeRef = useRef(0);
 
-  return <></>;
+  useEffect(() => {}, [isRunning]);
+
+  function Start() {}
+  function Stop() {}
+  function Reset() {}
+  function FormatTime() {
+    return "00:00:00";
+  }
+
+  return (
+    <>
+      <div className="stopwatch">
+        <div className="display">{FormatTime()}</div>
+        <div className="controls">
+          <button className="start-button" onClick={Start}>
+            Start
+          </button>
+          <button className="stop-button" onClick={stop}>
+            Stop
+          </button>
+          <button className="reset-button" onClick={Reset}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default StopWatch;
